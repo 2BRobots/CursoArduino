2d4610ddb0e9b685d00229481de8d9b93e205dc8
Daniel A. Fernandez R <dafrelectronics@outlook.com>
2016-03-29 15:18:35 -0600

    interrpciones

--- Practicas en clase/Interrupciones/Interrupciones.ino
+++ Practicas en clase/Interrupciones/Interrupciones.ino
@@ -3,11 +3,20 @@ void setup()
   pinMode(12, OUTPUT);
   pinMode(11, OUTPUT);
   pinMode(2, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(2), interrupcion, FALLING);
 
 }
 
 void loop()
 {
-
-
+digitalWrite(12, HIGH);
+delay(1000);
+digitalWrite(12, LOW);
+delay(1000);
 }
+
+void interrupcion()
+{
+digitalWrite(11, !(digitalRead(11)));  
+}
+
