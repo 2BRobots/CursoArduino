abbf3667462b62f0d910d2864ba247da7317faf2
Daniel A. Fernandez R <dafrelectronics@outlook.com>
2016-03-31 18:48:02 -0600

    1

--- Practicas en clase/Practica recreativa (tone)/marcha_imperial/marcha_imperial.ino
+++ Practicas en clase/Practica recreativa (tone)/marcha_imperial/marcha_imperial.ino
@@ -20,6 +20,7 @@
 #define gSH 830
 #define aH 880
 
+
 void setup() 
 {
   pinMode(pin, OUTPUT);
@@ -28,102 +29,170 @@ void setup()
 
 void loop() 
 {
-int re = 500;
-int re1 = 900;
+
+
+play();
+delay(5000);
 
 }
 void play()
 {
     tone(pin,a, 500);
+    delay(500);
     tone(pin,a, 500);
+    delay(500);
     tone(pin,a, 500);
+    delay(500);
     tone(pin,f, 350);
+    delay(350);
     tone(pin,cH, 150);
+    delay(150);
     tone(pin,a, 500);
+    delay(500);
     tone(pin,f, 350);
+    delay(350);
     tone(pin,cH, 150);
+    delay(150);
     tone(pin,a, 650);
+    delay(650);
  
     delay(150);
     //end of first bit
  
     tone(pin,eH, 500);
+    delay(500);
     tone(pin,eH, 500);
+    delay(500);
     tone(pin,eH, 500);
+    delay(500);
     tone(pin,fH, 350);
+    delay(350);
     tone(pin,cH, 150);
+    delay(150);
     tone(pin,gS, 500);
+    delay(500);
     tone(pin,f, 350);
+    delay(350);
     tone(pin,cH, 150);
+    delay(150);
     tone(pin,a, 650);
+    delay(650);
  
     delay(150);
     //end of second bit...
  
     tone(pin,aH, 500);
+    delay(500);
     tone(pin,a, 300);
+    delay(300);
     tone(pin,a, 150);
+    delay(150);
     tone(pin,aH, 400);
+    delay(400);
     tone(pin,gSH, 200);
+    delay(200);
     tone(pin,gH, 200);
+    delay(200);
     tone(pin,fSH, 125);
+    delay(125);
     tone(pin,fH, 125);
+    delay(125);
     tone(pin,fSH, 250);
+    delay(250);
  
     delay(250);
  
     tone(pin,aS, 250);
+    delay(250);
     tone(pin,dSH, 400);
+    delay(400);
     tone(pin,dH, 200);
+    delay(200);
     tone(pin,cSH, 200);
+    delay(200);
     tone(pin,cH, 125);
+    delay(125);
     tone(pin,b, 125);
+    delay(125);
     tone(pin,cH, 250);
+    delay(250);
  
     delay(250);
  
     tone(pin,f, 125);
+    delay(125);
     tone(pin,gS, 500);
+    delay(500);
     tone(pin,f, 375);
+    delay(375);
     tone(pin,a, 125);
+    delay(125);
     tone(pin,cH, 500);
+    delay(500);
     tone(pin,a, 375);
+    delay(375);
     tone(pin,cH, 125);
+    delay(125);
     tone(pin,eH, 650);
- 
+    delay(650);
+    
     //end of third bit... (Though it doesn't play well)
     //let's repeat it
  
     tone(pin,aH, 500);
+    delay(500);
     tone(pin,a, 300);
+    delay(300);
     tone(pin,a, 150);
+    delay(150);
     tone(pin,aH, 400);
+    delay(400);
     tone(pin,gSH, 200);
+    delay(200);
     tone(pin,gH, 200);
+    delay(200);
     tone(pin,fSH, 125);
+    delay(125);
     tone(pin,fH, 125);
+    delay(125);
     tone(pin,fSH, 250);
+    delay(250);
  
     delay(250);
  
     tone(pin,aS, 250);
+    delay(250);
     tone(pin,dSH, 400);
+    delay(400);
     tone(pin,dH, 200);
+    delay(200);
     tone(pin,cSH, 200);
+    delay(200);
     tone(pin,cH, 125);
+    delay(125);
     tone(pin,b, 125);
+    delay(125);
     tone(pin,cH, 250);
+    delay(250);
  
     delay(250);
  
     tone(pin,f, 250);
+    delay(250);
     tone(pin,gS, 500);
+    delay(500);
     tone(pin,f, 375);
+    delay(375);
     tone(pin,cH, 125);
+    delay(125);
     tone(pin,a, 500);
+    delay(500);
     tone(pin,f, 375);
+    delay(375);
     tone(pin,cH, 125);
+    delay(125);
     tone(pin,a, 650);
+    delay(650);
     //end of the song
  
 }
