34d4fc768bfe04fbbd6106c7ab195f37d927f411
Daniel A. Fernandez R <dafrelectronics@outlook.com>
2016-04-07 20:19:10 -0500

    visual studio

---
+++ Practicas en clase/Conexion con visual studio/WindowsApplication2/Form1.Designer.vb
@@ -0,0 +1,139 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.Button3 = New System.Windows.Forms.Button
+        Me.Button4 = New System.Windows.Forms.Button
+        Me.Button5 = New System.Windows.Forms.Button
+        Me.Button6 = New System.Windows.Forms.Button
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.SuspendLayout()
+        '
+        'SerialPort1
+        '
+        Me.SerialPort1.PortName = "COM24"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(160, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(112, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Conectar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(160, 41)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(112, 23)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Desconectar"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(146, 137)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(126, 23)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "Encender"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(12, 137)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(126, 23)
+        Me.Button5.TabIndex = 4
+        Me.Button5.Text = "Apagar"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.BackColor = System.Drawing.SystemColors.Control
+        Me.Button6.ForeColor = System.Drawing.Color.Transparent
+        Me.Button6.Location = New System.Drawing.Point(12, 70)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(260, 61)
+        Me.Button6.TabIndex = 5
+        Me.Button6.UseVisualStyleBackColor = False
+        '
+        'Timer1
+        '
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(17, 31)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 6
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 12)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(111, 13)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Seleccione un puerto:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 167)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "Form1"
+        Me.Opacity = 0.95
+        Me.Text = "Led por Puerto de Serie"
+        Me.TopMost = True
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents SerialPort1 As System.IO.Ports.SerialPort
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+
+End Class
---
+++ Practicas en clase/Conexion con visual studio/WindowsApplication2/Form1.vb
@@ -0,0 +1,115 @@
+﻿Public Class Form1
+    Dim sp As String
+    Dim msg As String
+
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Button6.BackColor = Color.Gray
+        Button3.Enabled = False
+        Timer1.Start()
+        For Each sp As String In My.Computer.Ports.SerialPortNames
+            ComboBox1.Items.Add(sp)
+        Next
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Try
+            SerialPort1.PortName = ComboBox1.Text()
+            SerialPort1.Open()
+            System.Threading.Thread.Sleep(200)
+            SerialPort1.Write("D")
+            System.Threading.Thread.Sleep(100)
+            sp = SerialPort1.ReadExisting()
+            If sp = "0" Then
+                Button6.BackColor = Color.Black
+            End If
+            If sp = "1" Then
+                Button6.BackColor = Color.Red
+            End If
+            Button3.Enabled = True
+            Button1.Enabled = False
+        Catch
+            MessageBox.Show("Ha ocurrido un error al abrir el puerto, verifique que el puerto seleccionado sea correcto.")
+
+        End Try
+
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        SerialPort1.Close()
+        Button6.BackColor = Color.Gray
+        Button3.Enabled = False
+        Button1.Enabled = True
+    End Sub
+
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+        If SerialPort1.IsOpen = False Then
+            msg = "Debe conectarse Primero"
+            MsgBox(msg)
+            Exit Sub
+        End If
+        SerialPort1.Write("B")
+        System.Threading.Thread.Sleep(100)
+        sp = SerialPort1.ReadExisting()
+        If sp = "0" Then
+            Button6.BackColor = Color.Black
+        End If
+        If sp = "1" Then
+            Button6.BackColor = Color.Red
+        End If
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        If SerialPort1.IsOpen = False Then
+            msg = "Debe conectarse Primero"
+            MsgBox(msg)
+            Exit Sub
+        End If
+        SerialPort1.Write("A")
+        System.Threading.Thread.Sleep(100)
+        sp = SerialPort1.ReadExisting()
+        If sp = "0" Then
+            Button6.BackColor = Color.Black
+        End If
+        If sp = "1" Then
+            Button6.BackColor = Color.Red
+        End If
+    End Sub
+
+    Private Sub Button6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button6.Click
+        If SerialPort1.IsOpen = False Then
+            msg = "Debe conectarse Primero"
+            MsgBox(msg)
+            Exit Sub
+        End If
+        SerialPort1.Write("C")
+        System.Threading.Thread.Sleep(100)
+        sp = SerialPort1.ReadExisting()
+        If sp = "0" Then
+            Button6.BackColor = Color.Black
+        End If
+        If sp = "1" Then
+            Button6.BackColor = Color.Red
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        If SerialPort1.IsOpen = True Then
+            SerialPort1.Write("D")
+            System.Threading.Thread.Sleep(100)
+            sp = SerialPort1.ReadExisting()
+            If sp = "0" Then
+                Button6.BackColor = Color.Black
+            End If
+            If sp = "1" Then
+                Button6.BackColor = Color.Red
+            End If
+        End If
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ComboBox1.SelectedIndexChanged
+  
+    End Sub
+End Class
+
+
